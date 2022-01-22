5c3014f03d5683911517f9e7542b45464444c8cc
Oded Dugma <odeddugma@gmail.com>
2022-01-22 13:31:14 +0200

    Method 2

--- Day 5 - Reverse A String/otherMethods.js
+++ Day 5 - Reverse A String/otherMethods.js
@@ -9,3 +9,17 @@ function method1(str) {
 }
 console.log(method1("hello"));
 console.log(method1("Howdy"));
+
+/* Method 2 */
+function method2(str) {
+	if (!str) return "No string specified";
+	let reverseString = "";
+	let i = str.length - 1;
+	while (i >= 0) {
+		reverseString += str[i];
+		i--;
+	}
+	return reverseString;
+}
+console.log(method2("ABCD"));
+console.log(method2("Oded"));
